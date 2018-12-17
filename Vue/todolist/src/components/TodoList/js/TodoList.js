68247cb27bfb7f41a753a76c0afb53fd58436d68
KWON\Administrator <kwyj0211@gmail.com>
2018-12-17 15:27:41 +0900

    [mod] Vue TodoList - Modify variable name

--- Vue/todolist/src/components/TodoList/js/TodoList.js
+++ Vue/todolist/src/components/TodoList/js/TodoList.js
@@ -27,8 +27,8 @@ export default {
     },
     createTodo(name){
       if(name !== null){
-        this.todo.push({name: name});
-        this.name = null;
+        this.todos.push({name: name});
+        this.addTodoName = null;
       }
     },
   },
