ddf097ba3a0f136ddeaacabbb0bcfceff07a92d2
KWON\Administrator <kwyj0211@gmail.com>
2019-02-13 18:00:22 +0900

    [add] Vue - Movie List Slide Animation

--- Vue/movie-list/src/components/MoviesList/MoviesList.vue
+++ Vue/movie-list/src/components/MoviesList/MoviesList.vue
@@ -1,15 +1,25 @@
 <template>
-    <ul class="movie-list-container">
-        <li v-for="item in list" class="movie-item">
-            <div class="movie-item-poster-wrap">
-                <img v-bind:src="item.poster" class="movie-item-poster"/>
-            </div>
+    <div class="movie-list-container">
+        <i class="material-icons slide-pre-btn" v-if="movieIdx > 0" v-on:click="preMovie">chevron_left</i>
 
-            <div class="movie-item-name">
-                {{item.name}}
-            </div>
-        </li>
-    </ul>
+        <div class="movie-item-wrap" ref="movieItemWrap">
+            <transition-group :name="direction">
+                <ul class="movie-item-container" :key="movieIdx" :style="{right : left + 'px'}">
+                    <li v-for="item in list" class="movie-item" :style="{width : containerWidth + 'px'}">
+                        <div class="movie-item-poster-wrap">
+                            <img v-bind:src="item.poster" class="movie-item-poster"/>
+                        </div>
+
+                        <div class="movie-item-name">
+                            {{item.name}}
+                        </div>
+                    </li>
+                </ul>
+            </transition-group>
+        </div>
+
+        <i class="material-icons slide-next-btn" v-if="movieIdx < movieCount-1" v-on:click="nextMovie">chevron_right</i>
+    </div>
 </template>
 
 <script type="text/javascript" src="./js/MoviesList.js"></script>
--- Vue/movie-list/src/components/MoviesList/js/MoviesList.js
+++ Vue/movie-list/src/components/MoviesList/js/MoviesList.js
@@ -1,5 +1,42 @@
 export default {
     props : ['list'],
+    data(){
+        return {
+            containerWidth : 0,
+            movieIdx : 0,
+            movieCount : 0,
+            direction : 'slide-right',
+            left : 0,
+        }
+    },
     methods:{
+        setMovieNum(){
+            this.movieCount = this.list.length;
+            return;
+        },
+        setContainerWidth() {
+            let width = this.$refs.movieItemWrap.clientWidth;
+            this.containerWidth = width;
+            return;
+        },
+        preMovie(){
+            this.direction = 'slide-left';
+            this.movieIdx -= 1;
+            this.left = this.containerWidth * this.movieIdx;
+            return;
+        },
+        nextMovie(){
+            this.direction = 'slide-right';
+            this.movieIdx += 1;
+            this.left = this.containerWidth * this.movieIdx;
+            return;
+        },
+    },
+    created() {
+        this.setMovieNum();
+        return;
+    },
+    mounted() {
+        this.setContainerWidth();
     },
 }
